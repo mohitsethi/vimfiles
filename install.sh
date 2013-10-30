13c2c8eaedd6fd9d16f0fbfa7a334078097b4004
mohitsethi <mohit@sethis.in>
2013-10-30 23:03:31 +0530

    Updated installation script

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
 cd ~/.vim_runtime
-cat ~/.vim_runtime/vimrcs/basic.vim > ~/.vimrc
+# cat ~/.vim_runtime/vimrcs/basic.vim > ~/.vimrc
+echo '
+source ~/.vim_runtime/vimrcs/basic.vim
+' > ~/.vimrc
 echo "Installed the Basic Vim configuration successfully! Enjoy :-)"
