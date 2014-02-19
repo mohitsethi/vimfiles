dd0fc14238f90baf46645130c603008658f2842e
mohitsethi <mohit@sethis.in>
2014-02-19 05:03:58 -0700

    Run vundle

--- install.sh
+++ install.sh
@@ -2,4 +2,6 @@ cd ~/.vim_runtime
 echo '
 source ~/.vim_runtime/vimrcs/basic.vim
 ' > ~/.vimrc
+echo "Configuring vim using Vundle..."
+vim +BundleInstall! +BundleClean +qa
 echo "Installed the Basic Vim configuration successfully! Enjoy :-)"
